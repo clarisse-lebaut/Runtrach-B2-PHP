f6b810d43f3a52c73ddd089242b6f98186d833d0
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-09-03 15:32:52 +0200

    get student data by an email in a form

--- jour02/job2.php
+++ jour02/job2.php
@@ -7,16 +7,13 @@ function connectBDD()
     //On fait un try catch pour attraper les erreur
     try {
         $connectBDD = new PDO('mysql:host=localhost;dbname=lp_official;charset=utf8', "root", "");
-        //Permet de vérifier qu'on a bien la connexion
-        echo 'Connexion réussi !';
         return $connectBDD; //* on stock la connexion pour l'utiliser ailleurs
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
     }
     ;
 }
-
-//Fonction pour trouver les étudiants
+//Fonction pour trouver un seul étudiant
 function find_one_student(string $email): array
 {
     //Connexion à la base donnée
@@ -36,13 +33,16 @@ function find_one_student(string $email): array
     //*on stock les données
     return $student;
 }
-
+?>
+<h2>Le var_dump des information d'un seul étudiant</h2>
+<?php
 $oneStudent = find_one_student('manon11@muller.org');
 var_dump($oneStudent);
 ?>
+
+<h2>Données d'un suel étudiante dans un tableau HTML</h2>
 <!-- Afficher les données dans un tableau HTML -->
 <table style="border:1px solid black">
-    <caption>Les informations d'un seul étudiant</caption>
     <tr>
         <?php
         foreach ($oneStudent as $columnKey => $value) {
@@ -59,5 +59,43 @@ var_dump($oneStudent);
         }
         ?>
     </tbody>
-
 </table>
+<br>
+
+<h2>Formulaire de récupération d'infomations d'un étudiant via son e-mail</h2>
+<!-- Récupérer les données depuis le mail dans un formulaire -->
+<div class="form">
+    <form class="formContent" action="job2.php" method="GET">
+        <br>
+        <label for="email">E-Mail recherché : </label>
+        <input id="email" name="email" type="email" required>
+        <button type="submit">Récupérer les informations de l'étudiant</button>
+    </form>
+</div>
+
+<?php
+// Vérifier si le formulaire a été soumis
+if (isset($_GET['email'])) {
+    //*Récupérer les données dans un formulaire
+    echo 'Email enregistré : ' . htmlspecialchars($email) . '<br>';
+    //* Récupérer les autres informations en fonction de l'email entrée dans l'input
+    //*TODO: récupérer l'email entrée dans l'input
+    $email = $_GET['email'];
+
+    //*TODO: chercher les valeurs dans le tableau associatif des données de la base de données
+    $getStudentData = find_one_student($email);
+
+    // var_dump($getStudentData);
+
+    // Vérifier si des données ont été récupérées
+    if ($getStudentData) {
+        echo "Nom & Prénom : " . htmlspecialchars($getStudentData['fullname']) . '<br>';
+        echo "Grade : " . htmlspecialchars($getStudentData['grade_id']) . '<br>';
+        echo "Email : " . htmlspecialchars($getStudentData['email']) . '<br>';
+        echo "Date de naissance : " . htmlspecialchars($getStudentData['birthdate']) . '<br>';
+        echo "Genre : " . htmlspecialchars($getStudentData['gender']) . '<br>';
+    } else {
+        echo "Aucun étudiant trouvé avec cet e-mail.";
+    }
+}
+?>
