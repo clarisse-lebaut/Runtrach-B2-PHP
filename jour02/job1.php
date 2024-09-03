412c5f09dedeffe057bc8702ae579096c40fce9a
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-09-03 11:18:44 +0200

    add print in html

--- jour02/job1.php
+++ jour02/job1.php
@@ -40,3 +40,44 @@ $student = find_all_student();
 //Print données dans un tableau assoiatif
 var_dump($student);
 ?>
+
+<!-- Afficher les données dans un tableau HTML -->
+<table sytle="border:1px solid black">
+    <caption>Tous les étudiants et leurs informations</caption>
+    <thead>
+        <tr sytle="background-color=lightgray;">
+            <!-- Afficher les en têtes des colonnes -->
+            <?php
+            /* 
+                *pour cette boucle, on itère sur un index
+                $student[0] --> on accède au premier enregistrement du tableau pour avoir les en-tête
+                $student --> tableau qui contient toute les enregistrements de la table student dans la BDD
+                as $columnKey => $value --> on itière sur chaque paire clé-valeur du premier enregistrement [0]
+                --> columKey c'est la clé (nom de la colonne) et value c'est la valeur
+            */
+
+            foreach ($student[0] as $columnKey => $value) {
+                //print l'en-tête du tableau, et on convertie les caractère spéciaux en les concaténant
+                echo '<th width="50px">' . htmlspecialchars($columnKey) . '</th>';
+            }
+            ?>
+        </tr>
+    </thead>
+    <tbody>
+        <!-- Afficher les données du tableau -->
+        <?php
+        //*TODO : ici on appelle la variable $student qui stock toute les données
+        //*TODO : et on crée $students pour itérer sur chaque valeur de l'enregistrement $student.
+        foreach ($student as $students) {
+            echo '<tr>'; // on met le début de la balise pour la ligne
+            //*TODO : ici on appelle la variable $students qui stock toute les données
+            //*TODO : et on crée $value pour itérer sur chaque valeur de l'enregistrement $students.
+            foreach ($students as $value) {
+                //print les cellules, et on convertie les caractère spéciaux en les concaténant
+                echo '<td>' . htmlspecialchars($value) . '</td>'; //on met la balise pour remplir chaque cellule avec les données
+            }
+            echo '</tr>'; //on ferme la balise des lignes
+        }
+        ?>
+    </tbody>
+</table>
